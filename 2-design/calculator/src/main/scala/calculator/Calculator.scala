932928252a904ea24c623ed534f59d7abfbfda4d
hogimn <hogimn@gmail.com>
2023-03-07 20:37:33 +0900

    Solution for calculator

--- 2-design/calculator/src/main/scala/calculator/Calculator.scala
+++ 2-design/calculator/src/main/scala/calculator/Calculator.scala
@@ -13,10 +13,19 @@ object Calculator extends CalculatorInterface:
 
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] =
-    ???
+    namedExpressions.view.mapValues(signal => Signal(eval(signal(), namedExpressions))).toMap
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]])(using Signal.Caller): Double =
-    ???
+    expr match {
+      case Literal(value) => value
+      case Plus(a, b) => eval(a, references) + eval(b, references)
+      case Minus(a, b) => eval(a, references) - eval(b, references)
+      case Times(a, b) => eval(a, references) * eval(b, references)
+      case Divide(a, b) => eval(a, references) / eval(b, references)
+      case Ref(name) =>
+        val referenceExpr = getReferenceExpr(name, references)
+        eval(referenceExpr, references - name)
+    }
 
   /** Get the Expr for a referenced variables.
    *  If the variable is not known, returns a literal NaN.
--- 2-design/calculator/src/main/scala/calculator/Polynomial.scala
+++ 2-design/calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,8 +3,22 @@ package calculator
 object Polynomial extends PolynomialInterface:
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] =
-    ???
+    Signal {
+      val aval = a()
+      val bval = b()
+      val cval = c()
+      bval * bval - 4 * aval * cval
+    }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] =
-    ???
+    Signal {
+      val aval = a()
+      val bval = b()
+      val deltaSqrt = Math.sqrt(computeDelta(a, b, c)())
+      if (deltaSqrt >= 0) {
+        Set((-1 * bval + deltaSqrt) / (2 * aval),
+            (-1 * bval - deltaSqrt) / (2 * aval))
+      }
+      else Set.empty
+    }
